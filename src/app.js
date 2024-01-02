682c1599c2aef8779d88954a10e19a6c58c3de88
vosscg <yot4106@gmail.com>
2024-01-02 11:23:33 +0800

    [feature]: customize cors

--- src/app.js
+++ src/app.js
@@ -1,24 +1,21 @@
 import "./loadEnv.js"
 import "./database.js";
 import { logRequestInfo } from "./middleware/request.js";
+import { corsMiddleware } from "./middleware/cors.js";
 import productRouter from './router/product.js';
 import express from 'express';
 import helmet from 'helmet'
-import cors from 'cors';
 
-const corsOption = {
-    origin: 'http://localhost:3000'
-}
 const app = express()
 
 // middleware
 app.use(helmet())
 app.use(express.json())
-app.use(cors(corsOption))
+app.use(corsMiddleware)
 app.use(logRequestInfo)
 
 // router
-app.use('/product',productRouter)
+app.use('/product', productRouter)
 
 
 app.listen(process.env.PORT)
---
+++ src/middleware/cors.js
@@ -0,0 +1,14 @@
+
+export const corsMiddleware = (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', "");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // 處理 OPTIONS 預檢請求
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+}
